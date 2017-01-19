e88a29404d4aee7c19b8b9d2fd0570a84130123f
Florian Walch <florian.walch@franka.de>
2017-01-19 14:06:11 +0100

    CMake: Include Google Test as subproject.
    Ensures that Google Test is built with the same compiler flags as the
    main project, fixing problems on Windows.

---
+++ cmake/SetupGoogleTest.cmake
@@ -0,0 +1,23 @@
+configure_file(${CMAKE_CURRENT_LIST_DIR}/GoogleTest-CMakeLists.txt.in
+               ${CMAKE_BINARY_DIR}/googletest-download/CMakeLists.txt)
+execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download
+)
+if(result)
+  message(FATAL_ERROR "Running CMake for Google Test failed: ${result}")
+endif()
+
+execute_process(COMMAND ${CMAKE_COMMAND} --build .
+  RESULT_VARIABLE result
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download
+)
+if(result)
+  message(FATAL_ERROR "Downloading Google Test failed: ${result}")
+endif()
+
+# Prevent overriding the parent project's compiler/linker
+# settings on Windows
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+add_subdirectory(${GTEST_SOURCE_DIR}/googlemock ${GTEST_BINARY_DIR} EXCLUDE_FROM_ALL)
