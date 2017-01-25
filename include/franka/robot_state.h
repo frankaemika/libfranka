47313b7df54fe865f3296881c8badc002c5bdc65
Florian Walch <florian.walch@franka.de>
2017-01-25 15:51:41 +0100

    Update RobotState.

--- include/franka/robot_state.h
+++ include/franka/robot_state.h
@@ -1,33 +1,26 @@
 #pragma once
 
-#include <boost/format.hpp>
-#include <cstdint>
-#include <iostream>
-#include <sstream>
+#include <array>
+#include <ostream>
 
 namespace franka {
+  struct RobotState {
+    std::array<double, 7> q_start;
+    std::array<double, 16> O_T_EE_start;
+    std::array<double, 2> elbow_start;
+    std::array<double, 7> tau_J;
+    std::array<double, 7> dtau_J;
+    std::array<double, 7> q;
+    std::array<double, 7> dq;
+    std::array<double, 7> q_d;
+    std::array<double, 7> joint_contact;
+    std::array<double, 6> cartesian_contact;
+    std::array<double, 7> joint_collision;
+    std::array<double, 6> cartesian_collision;
+    std::array<double, 7> tau_ext_hat_filtered;
+    std::array<double, 6> O_F_ext_hat_EE;
+    std::array<double, 6> EE_F_ext_hat_EE;
+  };
 
-struct RobotState {
-  std::uint32_t timestamp;
-  double q[7];
-  double dq[7];
-  double tau_J[7];
-  double dtau_J[7];
-
-  friend std::ostream& operator<<(std::ostream& os, const RobotState& rs);
-};
-
-template <typename T, size_t N>
-std::string joinArray(const T (&array)[N]) {
-  std::ostringstream oss;
-
-  oss << array[0];
-  for (unsigned int i = 1; i < N; i++) {
-    oss << ", " << array[i];
-  }
-  return oss.str();
-}
-
-std::ostream& operator<<(std::ostream& os, const franka::RobotState& rs);
-
+  std::ostream& operator<<(std::ostream& ostream, const franka::RobotState& robot_state);
 }  // namespace franka
