bee72e2df02492063c52bee47049bb6bd5dedc62
Florian Walch <florian.walch@franka.de>
2017-01-31 18:10:16 +0100

    CI: Enable strict mode, generate documentation only for release build.

--- scripts/ci/debug-build.sh
+++ scripts/ci/debug-build.sh
@@ -5,7 +5,7 @@ set -e
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DSTRICT=ON -DBUILD_COVERAGE=OFF -DBUILD_DOCUMENTATION=ON -DBUILD_EXAMPLES=ON -DBUILD_TESTS=ON ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_COVERAGE=OFF -DBUILD_DOCUMENTATION=OFF -DBUILD_EXAMPLES=ON -DBUILD_TESTS=ON ..
 cmake --build . --target check-format
 cmake --build .
 ctest -V
