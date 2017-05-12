3b7f1cbe16d6655a325b7497fd0c939473ea2a8d
Lukasz Duda <lukasz.duda@franka.de>
2017-05-12 17:55:36 +0200

    Library loading

---
+++ examples/use_model_library.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iterator>
+
+#include <franka/model.h>
+
+template <class T, size_t N>
+std::ostream& operator<<(std::ostream& ostream, const std::array<T, N>& array) {
+  ostream << "[";
+  std::copy(array.cbegin(), array.cend() - 1,
+            std::ostream_iterator<T>(ostream, ","));
+  std::copy(array.cend() - 1, array.cend(), std::ostream_iterator<T>(ostream));
+  ostream << "]";
+  return ostream;
+}
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: ./use_model_library <robot-hostname>" << std::endl;
+    return -1;
+  }
+
+  try {
+    franka::Robot robot(argv[1]);
+
+    franka::RobotState state = robot.readOnce();
+
+    franka::Model model(robot);
+    std::cout << model.jointPose(8, state) << std::endl;
+  } catch (franka::Exception const& e) {
+    std::cout << e.what() << std::endl;
+    return -1;
+  }
+
+  return 0;
+}
