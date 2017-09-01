bf358c64ded12b58f00e0ab5aa2b49e2a20d00d7
mo <mohamadreza.sabaghian@franka.de>
2017-09-01 18:59:10 +0200

    clang tidy warnings fixed

--- examples/joint_point_to_point_motion.cpp
+++ examples/joint_point_to_point_motion.cpp
@@ -1,9 +1,8 @@
+#include <franka/exception.h>
+#include <franka/robot.h>
 #include <cmath>
 #include <iostream>
 #include <vector>
-
-#include <franka/exception.h>
-#include <franka/robot.h>
 
 /**
  * @example joint_point_to_point_motion.cpp
@@ -13,10 +12,13 @@
  */
 
 inline int sgn(double x) {
-  if (x == 0)
-    return 0;
-  else
-    return (x > 0) ? 1 : -1;
+  int sign = 0;
+  if (x == 0) {
+    sign = 0;
+  } else {
+    sign = (x > 0) ? 1 : -1;
+  }
+  return sign;
 }
 void add(const std::array<double, 7>& a,
          const std::array<double, 7>& b,
@@ -32,7 +34,7 @@ void sub(const std::array<double, 7>& a,
     (*out)[i] = a[i] - b[i];
   }
 }
-void max_vector(const std::array<double, 7>& a, double* out) {
+void maxVector(const std::array<double, 7>& a, double* out) {
   (*out) = a[0];
   for (uint i = 0; i < a.size(); i++) {
     if (a[i] > (*out)) {
@@ -41,16 +43,16 @@ void max_vector(const std::array<double, 7>& a, double* out) {
   }
 }
 
-void calculation_of_desired_values(double t,
-                                   const std::array<double, 7>& delta_q,
-                                   const std::array<double, 7>& dq_max,
-                                   const std::array<double, 7>& t_1,
-                                   const std::array<double, 7>& t_2,
-                                   const std::array<double, 7>& t_f,
-                                   const std::array<double, 7>& q_1,
-                                   double delta_q_motion_finished,
-                                   std::array<double, 7>* delta_q_d,
-                                   bool* motion_finished_flag) {
+void calculationOfDesiredValues(double t,
+                                const std::array<double, 7>& delta_q,
+                                const std::array<double, 7>& dq_max,
+                                const std::array<double, 7>& t_1,
+                                const std::array<double, 7>& t_2,
+                                const std::array<double, 7>& t_f,
+                                const std::array<double, 7>& q_1,
+                                double delta_q_motion_finished,
+                                std::array<double, 7>* delta_q_d,
+                                bool* motion_finished_flag) {
   std::array<int, 7> sign_delta_q;
   std::array<double, 7> t_d;
   std::array<double, 7> delta_t_2;
@@ -100,7 +102,8 @@ void calculation_of_desired_values(double t,
 }
 int main(int argc, char** argv) {
   if (argc != 10) {
-    std::cerr << "Usage: ./generate_joint_pose_motion <robot-hostname> <goal_position> <speed factor(between zero and 1)>"
+    std::cerr << "Usage: ./generate_joint_pose_motion <robot-hostname> <goal_position> <speed "
+                 "factor(between zero and 1)>"
               << std::endl;
     return -1;
   }
@@ -116,14 +119,10 @@ int main(int argc, char** argv) {
     // control loop
     // Set collision behavior:
     robot.setCollisionBehavior(
-        {{20.0,  20.0,  20.0,  20.0,  20.0,  20.0,  20.0}},
-        {{20.0,  20.0,  20.0,  20.0,  20.0,  20.0,  20.0}},
-        {{10.0,  10.0,  10.0,  10.0,  10.0,  10.0,  10.0}},
-        {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}},
-        {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
-        {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
-        {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0}},
-        {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0}});
+        {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0}}, {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
+        {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}}, {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}},
+        {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0}}, {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
+        {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0}}, {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0}});
     // Reads the start position
     auto start_position = robot.readOnce().q_d;
     std::array<double, 7> q_start;
@@ -136,9 +135,9 @@ int main(int argc, char** argv) {
     std::array<double, 7> ddq_max_start = {5, 5, 5, 5, 5, 5, 5};
     std::array<double, 7> ddq_max_goal = {5, 5, 5, 5, 5, 5, 5};
     for (int joint_index = 0; joint_index < 7; joint_index++) {
-       dq_max[joint_index] = speed_factor * dq_max[joint_index];
-       ddq_max_start[joint_index] = speed_factor * ddq_max_start[joint_index];
-       ddq_max_goal[joint_index] = speed_factor * ddq_max_goal[joint_index];
+      dq_max[joint_index] = speed_factor * dq_max[joint_index];
+      ddq_max_start[joint_index] = speed_factor * ddq_max_start[joint_index];
+      ddq_max_goal[joint_index] = speed_factor * ddq_max_goal[joint_index];
     }
     double delta_q_motion_finished = 0.000001;
     double time = 0.0;
@@ -155,7 +154,6 @@ int main(int argc, char** argv) {
     std::array<double, 7> delta_q;
     std::array<double, 7> delta_q_d;
 
-    // double* delta_q_d[7];// = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
     bool motion_finished_flag;
     sub(q_goal, q_start, &delta_q);
     int sign_delta_q[7];
@@ -177,7 +175,7 @@ int main(int argc, char** argv) {
       }
     }
     double max_t_f;
-    max_vector(t_f, &max_t_f);
+    maxVector(t_f, &max_t_f);
     for (int joint_index = 0; joint_index < 7; joint_index++) {
       if (std::abs(delta_q[joint_index]) > delta_q_motion_finished) {  // moving joints
         double a = 1.5 / 2.0 * (ddq_max_goal[joint_index] + ddq_max_start[joint_index]);
@@ -195,24 +193,24 @@ int main(int argc, char** argv) {
             dq_max_sync[joint_index] * sign_delta_q[joint_index] * (0.5 * t_1_sync[joint_index]);
       }
     }
-    robot.control([=, &time, &delta_q_d, &motion_finished_flag](
-                      const franka::RobotState&,
-                      franka::Duration time_step) -> franka::JointPositions {
+    robot.control(
+        [=, &time, &delta_q_d, &motion_finished_flag](
+            const franka::RobotState&, franka::Duration time_step) -> franka::JointPositions {
 
-      time += time_step.s();
+          time += time_step.s();
 
-      calculation_of_desired_values(time, delta_q, dq_max_sync, t_1_sync, t_2_sync, t_f_sync, q_1,
-                                    delta_q_motion_finished, &delta_q_d, &motion_finished_flag);
+          calculationOfDesiredValues(time, delta_q, dq_max_sync, t_1_sync, t_2_sync, t_f_sync, q_1,
+                                     delta_q_motion_finished, &delta_q_d, &motion_finished_flag);
 
-      if (motion_finished_flag == true) {
-        std::cout << std::endl << "Finished motion, shutting down example" << std::endl;
-        return franka::Stop;
-      }
+          if (motion_finished_flag == true) {
+            std::cout << std::endl << "Finished motion, shutting down example" << std::endl;
+            return franka::Stop;
+          }
 
-      return {{q_start[0] + delta_q_d[0], q_start[1] + delta_q_d[1], q_start[2] + delta_q_d[2],
-               q_start[3] + delta_q_d[3], q_start[4] + delta_q_d[4], q_start[5] + delta_q_d[5],
-               q_start[6] + delta_q_d[6]}};
-    });
+          return {{q_start[0] + delta_q_d[0], q_start[1] + delta_q_d[1], q_start[2] + delta_q_d[2],
+                   q_start[3] + delta_q_d[3], q_start[4] + delta_q_d[4], q_start[5] + delta_q_d[5],
+                   q_start[6] + delta_q_d[6]}};
+        });
   } catch (const franka::Exception& e) {
     std::cout << e.what() << std::endl;
     return -1;
