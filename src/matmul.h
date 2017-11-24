b576e04abc348d8913623c2ca4bbfa63d565529f
Simon Gabl <simon.gabl@franka.de>
2017-11-24 12:08:08 +0100

    Template matMul.

--- src/matmul.h
+++ src/matmul.h
@@ -3,19 +3,21 @@
 #pragma once
 
 #include <array>
+#include <cmath>
 
 namespace franka {
 
 /**
  * Computes matrix multiplication C=A*B
  */
-inline std::array<double, 16> matMul(const std::array<double, 16>& a,
-                                     const std::array<double, 16>& b) {
-  std::array<double, 16> c{};
-  for (size_t row = 0; row < 4; row++) {
-    for (size_t col = 0; col < 4; col++) {
-      for (size_t inner = 0; inner < 4; inner++) {
-        c[row + col * 4] += a[row + inner * 4] * b[inner + col * 4];
+template <size_t T>
+inline std::array<double, T> matMul(const std::array<double, T>& a,
+                                     const std::array<double, T>& b) {
+  std::array<double, T> c{};
+  for (size_t row = 0; row < std::sqrt(T); row++) {
+    for (size_t col = 0; col < std::sqrt(T); col++) {
+      for (size_t inner = 0; inner < std::sqrt(T); inner++) {
+        c[row + col * std::sqrt(T)] += a[row + inner * std::sqrt(T)] * b[inner + col * std::sqrt(T)];
       }
     }
   }
