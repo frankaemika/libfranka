d3ee67ffd9af77b56f222c31226c383a1405b03d
Sebastian Jahr <sebastian.jahr@franka.de>
2019-01-25 15:14:25 +0100

    remove useless namespace and unused headers in platform_type.h

--- include/franka/platform_type.h
+++ include/franka/platform_type.h
@@ -2,26 +2,22 @@
 // Use of this source code is governed by the Apache-2.0 license, see LICENSE
 #pragma once
 
-#include <iostream>
-
 /**
  * @file platform_type.h
  * Defines supported target platform types.
  */
-namespace franka {
-  #if defined(__amd64__) || defined(_M_AMD64)
-        #define X64
-  #elif defined(__X86__) || defined(_M_IX86)
-        #define X86
-  #elif defined(__arm__) || defined(_M_ARM)
-        #define ARM
-  #elif defined(__aarch64__) || defined(_M_ARM64)
-        #define ARM64
-  #endif
+#if defined(__amd64__) || defined(_M_AMD64)
+      #define X64
+#elif defined(__X86__) || defined(_M_IX86)
+      #define X86
+#elif defined(__arm__) || defined(_M_ARM)
+      #define ARM
+#elif defined(__aarch64__) || defined(_M_ARM64)
+      #define ARM64
+#endif
 
-  #if defined(_WIN32) || defined(_WIN64)
-        #define WINDOWS
-  #elif defined(__unix) || defined(__unix__)
-        #define LINUX
-  #endif
-}  // namespace franka
+#if defined(_WIN32) || defined(_WIN64)
+      #define WINDOWS
+#elif defined(__unix) || defined(__unix__)
+      #define LINUX
+#endif
