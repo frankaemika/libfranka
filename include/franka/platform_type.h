32947f6e314218acb749e4fe631d3714ae45ab48
Sebastian Jahr <sebastian.jahr@franka.de>
2019-02-04 13:14:33 +0100

    Remove not supported architectures.

--- include/franka/platform_type.h
+++ include/franka/platform_type.h
@@ -10,10 +10,6 @@
       #define X64
 #elif defined(__X86__) || defined(_M_IX86)
       #define X86
-#elif defined(__arm__) || defined(_M_ARM)
-      #define ARM
-#elif defined(__aarch64__) || defined(_M_ARM64)
-      #define ARM64
 #endif
 
 #if defined(_WIN32) || defined(_WIN64)
