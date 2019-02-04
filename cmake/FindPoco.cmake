e37712bf2ab0ccf311d415b916647346171a7baf
Sebastian Jahr <sebastian.jahr@franka.de>
2019-02-04 10:52:04 +0100

    Fix formatting in cmake/FindPoco.cmake

--- cmake/FindPoco.cmake
+++ cmake/FindPoco.cmake
@@ -1,7 +1,8 @@
 find_package(Poco COMPONENTS ${Poco_FIND_COMPONENTS} CONFIG)
 if(Poco_FOUND)
-return()
+  return()
 endif()
+
 find_path(Poco_INCLUDE_DIR Poco/Poco.h)
 mark_as_advanced(FORCE Poco_INCLUDE_DIR)
 
