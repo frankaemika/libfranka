2193b735acd591a19d7c0dcf678eae2717a85f02
Sebastian Jahr <sebastian.jahr@franka.de>
2019-02-19 16:20:29 +0100

    Add more input parameter checks for low-pass filter

--- include/franka/lowpass_filter.h
+++ include/franka/lowpass_filter.h
@@ -26,12 +26,21 @@ constexpr double kDefaultCutoffFrequency = 100.0;
  * @param[in] y_last Value of the signal to be filtered in the previous time step
  * @param[in] cutoff_frequency Cutoff frequency of the low-pass filter
  * @throw std::invalid_argument if y is infinite or NaN.
+ * @throw std::invalid_argument if y_last is infinite or NaN.
+ * @throw std::invalid_argument if cutoff_frequency is negative, infinite or NaN.
+ * @throw std::invalid_argument if sample_time is negative, infinite or NaN.
  *
  * @return Filtered value.
  */
 inline double lowpassFilter(double sample_time, double y, double y_last, double cutoff_frequency) {
-  if (!std::isfinite(y)) {
-    throw std::invalid_argument("Commanding value is infinite or NaN.");
+  if (sample_time < 0 || !std::isfinite(sample_time)) {
+    throw std::invalid_argument("lowpass-filter: sample_time is negative, infinite or NaN.");
+  }
+  if (cutoff_frequency < 0 || !std::isfinite(cutoff_frequency)) {
+    throw std::invalid_argument("lowpass-filter: cutoff_frequency is negative, infinite or NaN.");
+  }
+  if (!std::isfinite(y) || !std::isfinite(y_last)) {
+    throw std::invalid_argument("lowpass-filter: current or past input value of the signal to be filtered is infinite or NaN.");
   }
   double gain = sample_time / (sample_time + (1.0 / (2.0 * M_PI * cutoff_frequency)));
   return gain * y + (1 - gain) * y_last;
