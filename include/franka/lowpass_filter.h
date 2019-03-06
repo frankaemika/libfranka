2a059b83bab5814ae3ba1430df9216905a0401f5
Sebastian Jahr <sebastian.jahr@franka.de>
2019-03-06 14:14:34 +0100

    Add Cartesian low-pass filter.

--- include/franka/lowpass_filter.h
+++ include/franka/lowpass_filter.h
@@ -2,6 +2,9 @@
 // Use of this source code is governed by the Apache-2.0 license, see LICENSE
 #pragma once
 
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
 #include <cmath>
 
 /**
@@ -50,4 +53,58 @@ inline double lowpassFilter(double sample_time, double y, double y_last, double 
   return gain * y + (1 - gain) * y_last;
 }
 
+/**
+ * Applies a first-order low-pass filter to a Cartesian translation and applies slerp to the
+ * rotation.
+ *
+ * @param[in] sample_time Sample time constant
+ * @param[in] y Current value of the Cartesian transformation matrix to be filtered
+ * @param[in] y_last Value of the Cartesian transformation matrix to be filtered in the previous
+ * time step
+ * @param[in] cutoff_frequency Cutoff frequency of the low-pass filter
+ *
+ * @throw std::invalid_argument if elements of y is infinite or NaN.
+ * @throw std::invalid_argument if elements of y_last is infinite or NaN.
+ * @throw std::invalid_argument if cutoff_frequency is zero, negative, infinite or NaN.
+ * @throw std::invalid_argument if sample_time is negative, infinite or NaN.
+ *
+ * @return Filtered Cartesian transformation matrix.
+ */
+
+inline std::array<double, 16> CartesianLowpassFilter(double sample_time,
+                                                     std::array<double, 16> y,
+                                                     std::array<double, 16> y_last,
+                                                     double cutoff_frequency) {
+  if (sample_time < 0 || !std::isfinite(sample_time)) {
+    throw std::invalid_argument(
+        "Cartesian lowpass-filter: sample_time is negative, infinite or NaN.");
+  }
+  if (cutoff_frequency <= 0 || !std::isfinite(cutoff_frequency)) {
+    throw std::invalid_argument(
+        "Cartesian lowpass-filter: cutoff_frequency is zero, negative, infinite or NaN.");
+  }
+  for (size_t i = 0; i < y.size(); i++) {
+    if (!std::isfinite(y[i]) || !std::isfinite(y_last[i])) {
+      throw std::invalid_argument(
+          "Cartesian lowpass-filter: current or past input value of the signal to be filtered is "
+          "infinite or "
+          "NaN.");
+    }
+  }
+  Eigen::Affine3d transform(Eigen::Matrix4d::Map(y.data()));
+  Eigen::Affine3d transform_last(Eigen::Matrix4d::Map(y_last.data()));
+  Eigen::Quaterniond orientation(transform.linear());
+  Eigen::Quaterniond orientation_last(transform_last.linear());
+
+  double gain = sample_time / (sample_time + (1.0 / (2.0 * M_PI * cutoff_frequency)));
+  transform.translation() =
+      gain * transform.translation() + (1.0 - gain) * transform_last.translation();
+  orientation = orientation.slerp(gain, orientation_last);
+
+  transform.linear() << orientation.normalized().toRotationMatrix();
+  std::array<double, 16> filtered_values{};
+  Eigen::Map<Eigen::Matrix4d>(&filtered_values[0], 4, 4) = transform.matrix();
+
+  return filtered_values;
+}
 }  // namespace franka
