14db3ccb9f9d57b8668348568320c01a066c3d88
Sebastian Jahr <sebastian.jahr@franka.de>
2019-03-08 15:46:59 +0100

    Move Cartesian low-pass filter function body into own source file.

---
+++ src/lowpass_filter.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2017 Franka Emika GmbH
+// Use of this source code is governed by the Apache-2.0 license, see LICENSE
+
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+#include<franka/lowpass_filter.h>
+
+namespace franka {
+std::array<double, 16> CartesianLowpassFilter(double sample_time,
+                                                     std::array<double, 16> y,
+                                                     std::array<double, 16> y_last,
+                                                     double cutoff_frequency) {
+  if (sample_time < 0 || !std::isfinite(sample_time)) {
+    throw std::invalid_argument(
+        "Cartesian lowpass-filter: sample_time is negative, infinite or NaN.");
+  }
+  if (cutoff_frequency <= 0 || !std::isfinite(cutoff_frequency)) {
+    throw std::invalid_argument(
+        "Cartesian lowpass-filter: cutoff_frequency is zero, negative, infinite or NaN.");
+  }
+  for (size_t i = 0; i < y.size(); i++) {
+    if (!std::isfinite(y[i]) || !std::isfinite(y_last[i])) {
+      throw std::invalid_argument(
+          "Cartesian lowpass-filter: current or past input value of the signal to be filtered is "
+          "infinite or "
+          "NaN.");
+    }
+  }
+  Eigen::Affine3d transform(Eigen::Matrix4d::Map(y.data()));
+  Eigen::Affine3d transform_last(Eigen::Matrix4d::Map(y_last.data()));
+  Eigen::Quaterniond orientation(transform.linear());
+  Eigen::Quaterniond orientation_last(transform_last.linear());
+
+  double gain = sample_time / (sample_time + (1.0 / (2.0 * M_PI * cutoff_frequency)));
+  transform.translation() =
+      gain * transform.translation() + (1.0 - gain) * transform_last.translation();
+  orientation_last = orientation_last.slerp(gain, orientation);
+
+  transform.linear() << orientation_last.normalized().toRotationMatrix();
+  std::array<double, 16> filtered_values{};
+  Eigen::Map<Eigen::Matrix4d>(&filtered_values[0], 4, 4) = transform.matrix();
+
+  return filtered_values;
+}
+}  // namespace franka
